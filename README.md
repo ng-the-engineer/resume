f3381278e771770ae235a1847355cb35be2857aa
Anthony Ng <engineer.anthony.ng@gmail.com>
2021-04-14 11:06:12 +0100

    Add video in modal for Travie

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# resume
+# Interactive resume in A4 layout
+
+### Deployment
+
+Simply open index.html with a browser
+
+### Development
+
+This project uses CSS and Javascript. The A4 layout is based on ( Project HTML Sheets of Paper)[https://github.com/delight-im/HTML-Sheets-of-Paper].
+
+#### Project Structure
+
+```
+.
+├── css
+│   ├── effect.css
+│   ├── effect.css
+│   └── sheets-of-paper.css
+├── files
+│   └── travie-pitchdeck.pdf
+├── images
+│   └── covid_web.png
+├── index.html
+├── LICENSE
+└── README.md
+```
+
+### Browser Compatibility
+
+Tested in Chrome
--- js/controls.js
+++ js/controls.js
@@ -8,10 +8,17 @@ const modalTravie = document.getElementById("modalTravie");
 document.getElementById("keyPrjTravie").onclick = () => modalTravie.style.display = "block";
 document.getElementById("closeModalTravie").onclick = () => modalTravie.style.display = "none";
 
+// AutoBid
+const modalAutobid = document.getElementById("modalAutobid");
+document.getElementById("allPrjAutobid").onclick = () => modalAutobid.style.display = "block";
+document.getElementById("closeModalAutobid").onclick = () => modalAutobid.style.display = "none";
+
 window.onclick = (event) => {
   if (event.target == modalCovidClean) {
     modalCovidClean.style.display = "none";
   } else if (event.target == modalTravie) {
     modalTravie.style.display = "none";
+  } else if (event.target == modalAutobid) {
+    modalAutobid.style.display = "none";
   }
 };
