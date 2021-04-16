f20173c1118b741d0d1da093b6e73dcf8dbec236
Anthony Ng <engineer.anthony.ng@gmail.com>
2021-04-16 14:19:54 +0100

    Add SC, TG, Palo web

--- js/controls.js
+++ js/controls.js
@@ -4,8 +4,22 @@ document.getElementById("jobMelco").onclick = () => {
   modalMelco.style.display = "block";
   document.getElementById("melcoDefault").click();
 }
-
 document.getElementById("closeModalMelco").onclick = () => modalMelco.style.display = "none";
+
+// SC
+const modalSC = document.getElementById("modalSC");
+document.getElementById("jobSC").onclick = () => modalSC.style.display = "block";
+document.getElementById("closeModalSC").onclick = () => modalSC.style.display = "none";
+
+// Palo IT
+const modalPalo = document.getElementById("modalPalo");
+document.getElementById("jobPalo").onclick = () => modalPalo.style.display = "block";
+document.getElementById("closeModalPalo").onclick = () => modalPalo.style.display = "none";
+
+// TG
+const modalTG = document.getElementById("modalTG");
+document.getElementById("jobTG").onclick = () => modalTG.style.display = "block";
+document.getElementById("closeModalTG").onclick = () => modalTG.style.display = "none";
 
 // CovidClean
 const modalCovidClean = document.getElementById("modalCovidClean");
@@ -23,14 +37,20 @@ document.getElementById("allPrjAutobid").onclick = () => modalAutobid.style.disp
 document.getElementById("closeModalAutobid").onclick = () => modalAutobid.style.display = "none";
 
 window.onclick = (event) => {
-  if (event.target == modalCovidClean) {
-    console.log('Covid Clean')
-    modalCovidClean.style.display = "none";
-  } else if (event.target == modalTravie) {
-    modalTravie.style.display = "none";
-  } else if (event.target == modalAutobid) {
-    modalAutobid.style.display = "none";
-  } else if (event.target == modalMelco) {
-    modalMelco.style.display = "none";    
+  switch(event.target) {
+    case modalCovidClean:
+      modalCovidClean.style.display = "none";
+    case modalTravie:
+      modalTravie.style.display = "none";
+    case modalAutobid:
+      modalAutobid.style.display = "none";
+    case modalMelco:
+      modalMelco.style.display = "none";
+    case modalTG:
+      modalTG.style.display = "none";
+    case modalPalo:
+      modalPalo.style.display = "none";
   }
 };
+
+
