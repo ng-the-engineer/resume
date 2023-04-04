2443560a8731e3515ca7a9838ee05b588c78e03b
Anthony Ng <engineer.anthony.ng@gmail.com>
2023-04-04 09:06:39 +0100

    Increase x and y axis scale

--- js/plot-overlay.js
+++ js/plot-overlay.js
@@ -9,14 +9,14 @@ const xLabelHeight = 50;
 d3.csv("https://api-vercel-umber.vercel.app/data",
 (data) => {
   // Add X axis
-  var x = d3.scaleLinear().domain([2002, 2023]).range([0, width]);
+  var x = d3.scaleLinear().domain([2002, 2024]).range([0, width]);
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
   // Add Y axis - Exp
-  const y = d3.scaleLinear().domain([0, 10]).range([height, 0]);
+  const y = d3.scaleLinear().domain([0, 11]).range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
   // Add a scale for bubble size
